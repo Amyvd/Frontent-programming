027a403206d847e1a8910bc4ad1dd245c90e51ad
Amyvd <30554@ma-web.nl>
2021-02-15 00:53:15 +0100

    opdrachten \ les 1- eigenschappen van objecten uitvoeren (stap 8)

--- Opdrachten/Les 1 - Eigenschappen van objecten uitvoeren/dataUitvoer.js
+++ Opdrachten/Les 1 - Eigenschappen van objecten uitvoeren/dataUitvoer.js
@@ -13,6 +13,23 @@ const geefDagWeek = (num) => {
         default: return num;
     }
 }
+const maandnaam = (num) => {
+    switch(num) {
+        case 0: return 'januari'; break;
+        case 1: return 'februari'; break;
+        case 2: return 'maart'; break;
+        case 3: return 'april'; break;
+        case 4: return 'mei'; break;
+        case 5: return 'juni'; break;
+        case 6: return 'juli'; break;
+        case 7: return 'augustus'; break;
+        case 8: return 'september'; break;
+        case 9: return 'oktober'; break;
+        case 10: return 'november'; break;
+        case 11: return 'december'; break;
+        default: return num;
+    }
+}
 
 const maakDatum = (num) => {
     // zet de string van de JS datum om in een betere vorm 
@@ -24,9 +41,8 @@ const maakDatum = (num) => {
     let jaar = datum.getFullYear();
     let uren = datum.getHours();
     let minuten = datum.getMinutes();
-
     return `${geefDagWeek(dagWeek)} <br>
-    ${dagMaand} ${maand} ${jaar} ${uren}:${minuten} `;
+    ${dagMaand} ${maandNaam(maand)} ${jaar} ${uren}:${minuten} `;
 }
 
 const uitvoeren = () => {
